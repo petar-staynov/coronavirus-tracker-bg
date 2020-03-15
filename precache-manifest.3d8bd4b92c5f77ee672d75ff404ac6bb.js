0a2f23dd8699b76a76c0d97717428d5681ef1eca
petar-staynov <petar.staynov@gmail.com>
2020-03-16 00:17:47 +0200

    Updates

---
+++ precache-manifest.3d8bd4b92c5f77ee672d75ff404ac6bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50521b0925cebaeea2490c44b97e94f",
+    "url": "/coronavirus-tracker-bg/index.html"
+  },
+  {
+    "revision": "98d7e776ecce7960c982",
+    "url": "/coronavirus-tracker-bg/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d197b110e17501217e96",
+    "url": "/coronavirus-tracker-bg/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "98d7e776ecce7960c982",
+    "url": "/coronavirus-tracker-bg/static/js/2.067e3322.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/coronavirus-tracker-bg/static/js/2.067e3322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d197b110e17501217e96",
+    "url": "/coronavirus-tracker-bg/static/js/main.e250e914.chunk.js"
+  },
+  {
+    "revision": "52fcee19567a76df90ee",
+    "url": "/coronavirus-tracker-bg/static/js/runtime-main.e4ed55ae.js"
+  }
+]);
