88b6f96997ce4a25f096327f87f61b26e6cc02cd
petar-staynov <petar.staynov@gmail.com>
2020-03-21 21:28:05 +0200

    Updates

---
+++ precache-manifest.f5d23d4f8916a8662dc82ca65091b066.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9e4edf997fdae8470af40fa8cbd780",
+    "url": "/coronavirus-tracker-bg/index.html"
+  },
+  {
+    "revision": "9a7addf9aad73a67cd78",
+    "url": "/coronavirus-tracker-bg/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "845ee7c5d3e4280985d0",
+    "url": "/coronavirus-tracker-bg/static/css/main.1eb80094.chunk.css"
+  },
+  {
+    "revision": "9a7addf9aad73a67cd78",
+    "url": "/coronavirus-tracker-bg/static/js/2.9056dd4f.chunk.js"
+  },
+  {
+    "revision": "7421c45d3010d16bb57a0083072ac3d7",
+    "url": "/coronavirus-tracker-bg/static/js/2.9056dd4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845ee7c5d3e4280985d0",
+    "url": "/coronavirus-tracker-bg/static/js/main.ad9f2cd9.chunk.js"
+  },
+  {
+    "revision": "52fcee19567a76df90ee",
+    "url": "/coronavirus-tracker-bg/static/js/runtime-main.e4ed55ae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coronavirus-tracker-bg/precache-manifest.5a6c6efa61009022e7b5893a2dac78f9.js"
+  "/coronavirus-tracker-bg/precache-manifest.f5d23d4f8916a8662dc82ca65091b066.js"
 );
 
 self.addEventListener('message', (event) => {
